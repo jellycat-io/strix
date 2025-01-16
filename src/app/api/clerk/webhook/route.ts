cd1ec42d4162cdf5d431ba5fd6a485f210fa9385
salty-max <max@jellycat.fr>
2025-01-16 02:18:43 +0100

    :sparkles: clerk setup

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,24 @@
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+  try {
+    const { data } = await req.json();
+    if (!data) {
+      throw new Error("No data received from clerk");
+    }
+
+    await db.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        imageUrl: data.image_url,
+      },
+    });
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error receiving clerk webhook:", err);
+  }
+};
